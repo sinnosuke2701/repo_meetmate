369d53fbe2715ee1fe4712865e127e6e7916361a
sinnosuke2701 <ehgml1111@naver.com>
2025-08-22 14:08:05 +0900

    new project

---
+++ meetmate/src/main/java/com/meetmate/MeetmateApplication.java
@@ -0,0 +1,13 @@
+package com.meetmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetmateApplication.class, args);
+	}
+
+}
---
+++ meetmate/src/test/java/com/meetmate/MeetmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meetmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
